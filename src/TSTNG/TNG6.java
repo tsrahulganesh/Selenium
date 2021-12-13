75539ec1c8db17ef36a115c4cdd8cffed25a7d64
tsrahulganesh@gmail.com <kale%2493>
2021-12-13 14:49:04 +0530

    Testng

--- src/TSTNG/TNG6.java
+++ src/TSTNG/TNG6.java
@@ -1,5 +1,6 @@
 package TSTNG;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TNG6 {
@@ -8,12 +9,17 @@ public class TNG6 {
     public void classoneTest1(){
         System.out.println("classoneTest1");
     }
-    @Test
+    @Test(groups ="email")
     public void classoneTest2(){
         System.out.println("classonetest2");
     }
+
+    @Parameters ({"myurl","username","password"})
     @Test
-    public void classoneTest3(){
+    public void classoneTest3(String url,String user,String pass){
         System.out.println("classonetest3");
+        System.out.println("url="+url);
+        System.out.println("user="+user);
+        System.out.println("pass="+pass);
     }
 }
--- src/TSTNG/TNG8.java
+++ src/TSTNG/TNG8.java
@@ -2,6 +2,7 @@ package TSTNG;
 
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TNG8 {
@@ -15,9 +16,16 @@ public class TNG8 {
         public void anotherBeforeSuite(){
             System.out.println("another before suite");
         }
+
+        @Parameters({"myurl","username","password"})
         @AfterSuite
-        public void classThreeTest3(){
+        public void classThreeTest3(String url,String user,String pass)
+        {
             System.out.println("After my suite");
+            System.out.println("url="+url);
+            System.out.println("user="+user);
+            System.out.println("pass="+pass);
+
         }
     }
 
